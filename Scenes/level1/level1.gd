2375707b9ea65c1f2e4a0f0a6848bee904a96ff9
KGr1sha <krylov.grigory@gmail.com>
2023-02-22 17:53:37 +0300

    iasdasd

--- Scenes/level1/level1.gd
+++ Scenes/level1/level1.gd
@@ -4,35 +4,54 @@ var beforeBridgeChecker = false
 var beforeBridgeTalkedChecker = false
 var level1FinishedChecker = false
 var timerForCollectItems = 1
+var items_collected = false
+var near_bridge = false
+var bridge_builded = false
+
 
 func _ready():
 	get_node("Bridge/Sprite").hide()
 
 
 func _process(_delta):
-	if Global.cntLevel1 == 8 and level1FinishedChecker == false:
-		get_node("Bridge/CollisionShape2D").disabled = false
-		get_node("Bridge/Sprite").show()
-		get_node("CLOSED/CollisionShape2D").disabled = true
-		level1FinishedChecker = true
+	if near_bridge and items_collected and Input.is_action_just_pressed("use_items") and not bridge_builded:
+		build()
 
 
 func _on_beforeBridge_body_entered(body):
-	if Global.cntLevel1 == 8:
-		yield(get_tree().create_timer(5), "timeout")
-		beforeBridgeChecker = true
-		get_node("beforeBridge/CollisionShape2D").disabled = true
-	elif Global.cntLevel1 >= 5:
-		# show massage about the player did not find one item
-		yield(get_tree().create_timer(timerForCollectItems), "timeout")
-	elif Global.cntLevel1 >= 2:
-		# the player did not find two item
-		yield(get_tree().create_timer(timerForCollectItems), "timeout")
-	elif Global.cntLevel1 >= 0:
-		if beforeBridgeTalkedChecker:
-			# the player did not find anything
-			yield(get_tree().create_timer(timerForCollectItems), "timeout")
-		else:
-			beforeBridgeTalkedChecker = true
-			# show massage about quest on this level with timer on 5 secs
-			yield(get_tree().create_timer(timerForCollectItems), "timeout")
+	near_bridge = true
+	var inventory = PlayerInventory.inventory
+	var needed_items = []
+	for i in inventory:
+		if not inventory[i][1]:
+			needed_items.append(inventory[i][0])
+	if needed_items:
+		var s = ''
+		for i in range(len(needed_items)):
+			if i != len(needed_items) - 1:
+				s += needed_items[i] + ', '
+			else:
+				s += needed_items[i]
+		$Bridge/Tip.text = 'You need ' + s + ' to build the bridge'
+		$Bridge/Tip.show()
+	else:
+		if bridge_builded == false:
+			$Bridge/Tip.text = 'E - build the bridge'
+			$Bridge/Tip.show()
+			items_collected = true
+		
+
+
+func _on_beforeBridge_body_exited(body):
+	near_bridge = false
+	$Bridge/Tip.hide()
+	
+	
+func build():
+	$Bridge/Tip.hide()
+	bridge_builded = true
+	#Тут надо построить мост
+	
+	pass
+	
+	
