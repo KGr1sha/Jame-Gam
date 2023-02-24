ee919fdcdc4cbfbb99b567d26f13e7c255e7d690
KGr1sha <krylov.grigory@gmail.com>
2023-02-24 11:43:29 +0300

    add controls in main menu

--- Scenes/MainMenu/SettingsButton.gd
+++ Scenes/MainMenu/SettingsButton.gd
@@ -8,3 +8,7 @@ func _on_SettingsButton_pressed():
 func _on_StartButton_pressed():
 	Global.freeze(0)
 	get_tree().change_scene("res://Scenes/StartScene/StartScene.tscn")
+
+
+func _on_ControlsButton_pressed():
+	get_tree().change_scene("res://Scenes/MainMenu/Controls.tscn")
